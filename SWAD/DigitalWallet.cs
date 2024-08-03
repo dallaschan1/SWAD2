a964d49f6270387a0224f2859ee2cf4d34e1e3fc
awzhenkang <s10241668@connect.np.edu.sg>
2024-08-03 16:45:40 +0800

    done with Car and DigitalWallet classes

---
+++ SWAD/DigitalWallet.cs
@@ -0,0 +1,24 @@
+﻿namespace SWAD
+{
+    internal class DigitalWallet
+    {
+        public string WalletProvider { get; set; }
+
+        public string ProviderUserId { get; set; }
+
+        public string Password { get; set; }
+
+        public string WalletEmail { get; set; }
+
+        public string? WalletPhoneNumber { get; set; }
+
+        public DigitalWallet(string walletProvider, string providerUserId, string password, string walletEmail, string? walletPhoneNumber)
+        {
+            WalletProvider = walletProvider;
+            ProviderUserId = providerUserId;
+            Password = password;
+            WalletEmail = walletEmail;
+            WalletPhoneNumber = walletPhoneNumber;
+        }
+    }
+}
