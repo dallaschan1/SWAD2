4f38615dc6216e45ed6dde1c03c5a20807e004e7
edison <s10244576@connect.np.edu.sg>
2024-08-04 14:27:21 +0800

    added admin class

---
+++ SWAD/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_RegisterAcc
+{
+    internal class Admin : Account
+    {
+        private int adminId;
+
+        public int AdminId;
+
+        public Admin(int adminId)
+        {
+            AdminId = adminId;
+        }
+    }
+}
