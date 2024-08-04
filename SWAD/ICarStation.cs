7ecc0078749eda4d25909817139204430718941c
Chong Yun Ze <S10257927@connect.np.edu.sg>
2024-08-04 15:09:43 +0800

    icarstation class

---
+++ SWAD/ICarStation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD
+{
+    internal class ICarStation
+    {
+        private int stationId;
+        private string stationName;
+        private string stationAddress;
+        public int StationId { get; set; }
+        public string StationName { get; set; }
+        public string StationAddress { get; set;}
+        public ICarStation(int stationId, string stationName, string stationAddress)
+        {
+            StationId = stationId;
+            StationName = stationName;
+            StationAddress = stationAddress;
+            
+        }
+    }
+    
+}
