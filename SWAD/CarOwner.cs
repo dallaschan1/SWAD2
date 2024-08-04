da3401c684650646e068bcb5b9f75149e725d28a
Chong Yun Ze <S10257927@connect.np.edu.sg>
2024-08-04 18:58:23 +0800

    carowner class edit

--- SWAD/CarOwner.cs
+++ SWAD/CarOwner.cs
@@ -12,7 +12,7 @@ namespace Assignment2
         private int carOwnerid;
 
         public int CarOwnerId { get; set; }
-        public Car? Car { get; set; }
+        public List<Car> Cars { get; set; } = new List<Car>();
 
         public CarOwner(string username, string password, string email, string fullName, string phoneNo, int carOwnerid)
             : base(username, password, email, fullName, phoneNo)
